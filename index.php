cc978fc5b559b08d6267e8c2f2be6e95659614e9
Robbie Lopez <rolopez.email@gmail.com>
2017-05-26 10:48:09 -0600

    got the buttons working with the navigator displaying when you minimize

--- index.php
+++ index.php
@@ -5,15 +5,32 @@
 	<head>
 
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-
+		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-		<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-
+		<!-- My Custom CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+
+
+
+
+
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 
 
 
@@ -29,19 +46,28 @@
 
 		<section>
 
+		<nav class="navbar navbar-default">
+
 		<div class="container-fluid">
 
-			<div class="row">
+			<div class="navbar-header">
 
-				<div class="col-4"></div>
-					<div>
-					<p>LLdev</p>
-					</div>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+			</div>
 
 
-				<div class="col-8S">
 
-					<ul>
+
+				<div class="collapse navbar-collapse" id="main-menu">
+
+					<ul class="nav navbar-nav navbar-left">
 
 						<li>
 							<button id="aboutButt">About</button>
@@ -65,7 +91,9 @@
 
 			</div>
 
-		</div>
+
+
+		</nav>
 
 		</section>
 
