560d5bfb4a84a7427c8ec480bc0340f1d1b2f152
Robbie Lopez <rolopez.email@gmail.com>
2017-06-01 13:53:17 -0600

    time to push working on conatct form - added google recaptcha

--- index.php
+++ index.php
@@ -167,65 +167,64 @@
 
 						<div id="contactsContent"> <h3>Contact Me:</h3>
 
-							<!--Begin Contact Form-->
-							<form id="contact-form" action="php/mailer.php" method="post">
+							<!-- BEGIN CONTACT FORM -->
+							<form id="contact-form" method="POST" action="php/mailer.php">
 								<div class="form-group">
-									<label for="name">Name <span class="text-danger">*</span></label>
+									<label for="name">Name</label>
 									<div class="input-group">
 										<div class="input-group-addon">
-											<i class="fa fa-user" aria-hidden="true"></i>
+											<i class="fa fa-user"></i>
 										</div>
-										<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+										<input class="form-control" type="text" name="name" id="name" placeholder="Your Name">
 									</div>
 								</div>
 								<div class="form-group">
-									<label for="email">Email <span class="text-danger">*</span></label>
+									<label for="email">Email</label>
 									<div class="input-group">
 										<div class="input-group-addon">
-											<i class="fa fa-envelope" aria-hidden="true"></i>
+											<i class="fa fa-envelope"></i>
 										</div>
-										<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+										<input class="form-control" type="email" name="email" id="email" placeholder="Your Email">
 									</div>
 								</div>
 								<div class="form-group">
 									<label for="subject">Subject</label>
 									<div class="input-group">
 										<div class="input-group-addon">
-											<i class="fa fa-pencil" aria-hidden="true"></i>
+											<i class="fa fa-pencil"></i>
 										</div>
-										<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+										<input class="form-control" type="text" name="subject" id="subject" placeholder="Subject">
 									</div>
 								</div>
-
-
-
-
 								<div class="form-group">
-									<label for="message">Message <span class="text-danger">*</span></label>
+									<label for="message">Message</label>
 									<div class="input-group">
 										<div class="input-group-addon">
-											<i class="fa fa-comment" aria-hidden="true"></i>
+											<i class="fa fa-comment"></i>
 										</div>
-										<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+										<textarea rows="5" name="message" id="message" class="form-control" placeholder="Your Message (2000 charaters max)"></textarea>
 									</div>
 								</div>
 
-								<!-- reCAPTCHA -->
-								<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+								<!-- Google reCAPTCHA-->
+								<div class="g-recaptcha" data-sitekey="6LdF0iMUAAAAAL4NAq0zjr26SUZcktpRP4xhyuPd"></div>
 
-								<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-								<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+								<button class="btn btn-default" type="reset">Reset</button>
+								<button class="btn btn-info" type="submit">Submit</button>
 							</form>
+							<!-- END CONTACT FORM-->
 
-							<!--empty area for form error/success output-->
-							<div class="row">
-								<div class="col-xs-12">
-									<div id="output-area"></div>
-								</div>
-							</div>
-							</div>
+						</div><!-- /.col-sm-6 -->
+					</div><!-- /.row -->
+
+					<!-- EMPTY FORM OUTPUT AREA -->
+					<div class="row">
+						<div class="col-md-6">
+							<div id="output-area"></div>
 						</div>
 					</div>
+
+
 				</div>
 		</SECTION>
 
