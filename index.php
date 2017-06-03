8983f12879e6c720f0aea4602ab2a0b25e4c5aff
Robbie Lopez <rolopez.email@gmail.com>
2017-06-03 10:42:33 -0600

    trying to get pictures in within a cirlce

--- index.php
+++ index.php
@@ -157,78 +157,35 @@
 						</div><br><br>
 
 						<div id="busidevContentb"> <h3>Past Experiences:</h3>
-							<section>
-								<div class="container">
-									<div class="row">
-										<div class="col-md-3 text-center">
-											<!-- icon -->
-											<span class="fa-stack fa-5x">
-								<i class="fa fa-circle fa-stack-2x"></i>
-								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
-							</span>
+							<div class="container-fluid">
+								<div class="row">
+									<div class="col-md-2">
+										<img class="img-circle" src="kitten.jpg">
+									</div>
 
-											<!-- panel -->
-											<div class="panel panel-default">
-												<div class="panel-body">
-													<h3>Humblebrag</h3>
-													<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
-												</div>
-											</div>
-										</div><!--/.col-md-3 -->
+									<div class="col-md-2">
+										<img class="img-circle" src="kitten.jpg">
+									</div>
 
-										<div class="col-md-3 text-center">
-											<!-- icon -->
-											<span class="fa-stack fa-5x">
-								<i class="fa fa-circle fa-stack-2x"></i>
-								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
-							</span>
+									<div class="col-md-2">
+										<img class="img-circle" src="kitten.jpg">
+									</div>
 
-											<!-- panel -->
-											<div class="panel panel-default">
-												<div class="panel-body">
-													<h3>Humblebrag</h3>
-													<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
-												</div>
-											</div>
-										</div><!--/.col-md-3 -->
+									<div class="col-md-2">
+										<img class="img-circle" src="kitten.jpg">
+									</div>
 
-										<div class="col-md-3 text-center">
-											<!-- icon -->
-											<span class="fa-stack fa-5x">
-								<i class="fa fa-circle fa-stack-2x"></i>
-								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
-							</span>
+									<div class="col-md-2">
+										<img class="img-circle" src="kitten.jpg">
+									</div>
 
-											<!-- panel -->
-											<div class="panel panel-default">
-												<div class="panel-body">
-													<h3>Humblebrag</h3>
-													<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
-												</div>
-											</div>
-										</div><!--/.col-md-3 -->
-
-										<div class="col-md-3 text-center">
-											<!-- icon -->
-											<span class="fa-stack fa-5x">
-								<i class="fa fa-circle fa-stack-2x"></i>
-								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
-							</span>
-
-											<!-- panel -->
-											<div class="panel panel-default">
-												<div class="panel-body">
-													<h3>Humblebrag</h3>
-													<p>Selvage typewriter forage, craft beer vape glossier gastropub 3 wolf moon intelligentsia.</p>
-												</div>
-											</div>
-										</div><!--/.col-md-3 -->
-									</div><!--/.row-->
-								</div><!--/.container-->
-							</section>
+									<div class="col-md-2">
+										<img class="img-circle" src="kitten.jpg">
+									</div>
 
 
-
+								</div>
+							</div>
 						</div>
 
 						<div id="contactsContent"> <h3>Contact Me:</h3>
